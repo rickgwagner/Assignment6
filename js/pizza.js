cb220469b5cc5ff0f43744f551141390bef78731
Rick Wagner <rickw@fireoverheaven.com>
2015-10-21 17:12:05 -0700

    Files as of Wednesday PM

--- js/pizza.js
+++ js/pizza.js
@@ -26,8 +26,8 @@ if (window.addEventListener) {
 
 function validate() {
     "use strict";
-    var theName = document.pizzaForm.name.value;
-    var nameExp = /^([a-zA-Z ]){2,30}$/;
+    var theName = document.pizzaForm.name.value, nameExp = /^([a-zA-Z ]){2,30}$/, theState = document.pizzaForm.state.value, stateExp = /[0-9]/, zipExp = /^[0-9]{5}(?:-[0-9]{4})?$/, theZip = document.pizzaForm.zipcode.value, phoneExp = /^(()?\d{3}())?(-|\s)?\d{3}(-|\s)?\d{4}$/, thePhone = document.pizzaForm.phone.value, emailExp = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/, theEmail = document.pizzaForm.email.value;
+
     if (nameExp.test(theName) === false) {
         window.alert("Please provide your name");
         document.pizzaForm.name.focus();
@@ -43,29 +43,21 @@ function validate() {
         document.pizzaForm.city.focus();
         return false;
     }
-    var theState = document.pizzaForm.state.value;
-    var stateExp = /[0-9]/;
     if (document.pizzaForm.state.value === "" || document.pizzaForm.state.value.length !== 2 || stateExp.test(theState) === true) {
         window.alert("Please enter a state in 2 letters");
         document.pizzaForm.state.focus();
         return false;
     }
-    var zipExp = /^[0-9]{5}(?:-[0-9]{4})?$/;
-    var theZip = document.pizzaForm.zipcode.value;
     if (document.pizzaForm.zipcode.value === "" || zipExp.test(theZip) === false) {
         window.alert("Please provide a valid zipcode.");
         document.pizzaForm.zipcode.focus();
         return false;
     }
-    var phoneExp = /^(()?\d{3}())?(-|\s)?\d{3}(-|\s)?\d{4}$/;
-    var thePhone = document.pizzaForm.phone.value;
     if (document.pizzaForm.phone.value === "" || phoneExp.test(thePhone) === false) {
         window.alert("Please provide your Phone number");
         document.pizzaForm.phone.focus();
         return false;
     }
-    var emailExp = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
-    var theEmail = document.pizzaForm.email.value;
     if (document.pizzaForm.email.value === "" || emailExp.test(theEmail) === false) {
         window.alert("Please provide your Email");
         document.pizzaForm.email.focus();
@@ -73,7 +65,6 @@ function validate() {
     }
     return (true);
 }
-
 ////  Run validate function when leaving name field
 //document.getElementById("name").addEventListener("blur", function () {
 //    "use strict";
@@ -86,7 +77,6 @@ function validate() {
 //    validate();
 //});
 //
-
 //  Run validate function when leaving email field
 document.getElementById("email").addEventListener("blur", function () {
     "use strict";
@@ -95,26 +85,26 @@ document.getElementById("email").addEventListener("blur", function () {
 
 //  PRICES
 var handTossed = {
-    choose: "Choose a Size (0)",
+    choose: "0. Choose a Size",
     small: "Small ($9.99)",
     medium: "Medium ($12.99)",
     large: "Large ($14.99)"
 };
 
 var thinCrust = {
-    choose: "Choose a Size (0)",
+    choose: "0. Choose a Size",
     medium: "Medium ($11.99)",
     large: "Large ($13.99)"
 };
     
 var newYork = {
-    choose: "Choose a Size (0)",
+    choose: "0. Choose a Size",
     large: "Large ($16.99)",
     exlarge: "Extra Large ($19.99)"
 };
     
 var glutenFree = {
-    choose: "Choose a Size (0)",
+    choose: "0 . Choose a Size",
     small: "Small ($10.99)"
 };
 
@@ -134,8 +124,7 @@ var saucePrices = {
 // Add Size and Cost Options of Selected Crust to Dropdown list
 function getRadioVal(form, dough) {
     "use strict";
-    var val;
-    var radios = form.elements[dough];
+    var val, i = 0, len = 0, radios = form.elements[dough];
     for (var i = 0, len = radios.length; i < len; i++) {
         if ( radios[i].checked ) {
             val = radios[i].value;
@@ -145,49 +134,42 @@ function getRadioVal(form, dough) {
     return val;
 }
 
-document.getElementById('pizza').onchange = function() {
-var dropdown = document.getElementById("sizeOptions");
-var val = getRadioVal(this, 'dough');
-    
-var list = document.getElementById("sizeOptions");
+document.getElementById('pizza').onchange = function () {
+    var dropdown = document.getElementById("sizeOptions"), val = getRadioVal(this, 'dough'), list = document.getElementById("sizeOptions"), select = document.getElementById("sizeOptions");
+
 // As long as <select list> has a child node, remove it
-while (list.hasChildNodes()) {   
-    list.removeChild(list.firstChild);
-}     
-    
-if (val === "handTossed") {
-var select = document.getElementById("sizeOptions");
-for(index in handTossed) {
-select.options[select.options.length] = new Option(handTossed[index]);
-}
-}
-else if (val === "thinCrust") {
-var select = document.getElementById("sizeOptions");
-for(index in thinCrust) {
-select.options[select.options.length] = new Option(thinCrust[index]);
-}
-}
-else if (val === "newYork") {
-var select = document.getElementById("sizeOptions");
-for(index in newYork) {
-select.options[select.options.length] = new Option(newYork[index]);
-}
-}
-else {
-var select = document.getElementById("sizeOptions");
-for(index in glutenFree) {
-select.options[select.options.length] = new Option(glutenFree[index]);
-} 
-}
-}
+    while (list.hasChildNodes()) {
+        list.removeChild(list.firstChild);
+    }
+    if (val === "handTossed") {
+        for (index in handTossed) {
+            select.options[select.options.length] = new Option(handTossed[index], index);
+        }
+    } else if (val === "thinCrust") {
+//        var select = document.getElementById("sizeOptions");
+        for (index in thinCrust) {
+            select.options[select.options.length] = new Option(thinCrust[index], index);
+        }
+    } else if (val === "newYork") {
+//        var select = document.getElementById("sizeOptions");
+        for (index in newYork) {
+            select.options[select.options.length] = new Option(newYork[index], index);
+        }
+    } else {
+//        var select = document.getElementById("sizeOptions");
+        for (index in glutenFree) {
+            select.options[select.options.length] = new Option(glutenFree[index], index);
+        }
+    }
+};
 
 //  Show Cheese, Sauce and Topping Options once a Dough Option is Selected
 var hideExtras = document.getElementById("extras");
 hideExtras.classList.add("extras");
 
 document.getElementById("sizeOptions").addEventListener("change", function () {
-var showExtras = document.getElementById("extras");
-showExtras.classList.remove("extras");
+    var showExtras = document.getElementById("extras");
+    showExtras.classList.remove("extras");
 });
 
 //  Show Billing Info once Finished Building Pizza is Confirmed
@@ -195,30 +177,24 @@ var hideBilling = document.getElementById("billingInfo");
 hideBilling.classList.add("hideBilling");
 
 document.getElementById("finishedBuilding").addEventListener("click", function () {
-var confirmOptions = window.confirm("Are You Sure You're Done?");
-if (confirmOptions === true) {
-var showBilling = document.getElementById("billingInfo");
-showBilling.classList.remove("hideBilling");
-} 
+    var confirmOptions = window.confirm("Are You Sure You're Done?");
+    if (confirmOptions === true) {
+        var showBilling = document.getElementById("billingInfo");
+        showBilling.classList.remove("hideBilling");
+    } else {
+        x = "You pressed Cancel!";
+    }
 });
                                                              
 //  GETTING COSTS
-function getCrustPrice()
-{
-    var crustPrice = 0;
-    var selectedCrust = sizeOptions.options[sizeOptions.selectedIndex].text;
-    crustText = selectedCrust.replace (/[^\d.-]/g, '');
-    var crustPrice = parseFloat(crustText);   
+function getCrustPrice() {
+    var crustPrice = 0, selectedCrust = sizeOptions.options[sizeOptions.selectedIndex].text;
+    var crustText = selectedCrust.replace(/[^\d.-]/g, '');
+    var crustPrice = parseFloat(crustText);
     return crustPrice;
 }
 
-document.getElementById("sizeOptions").addEventListener("change", function () {
-    "use strict";
-    calculateTotal();
-});
-
-function getCheesePrice()
-{
+function getCheesePrice() {
     var cheesePrice = 0;
     var theForm = document.forms.pizza;
     var selectedCheese = theForm.elements.cheese;
@@ -226,13 +202,7 @@ function getCheesePrice()
     return cheesePrice;
 }
 
-document.getElementById("cheese").addEventListener("change", function () {
-    "use strict";
-    getCheesePrice(), calculateTotal();
-});
-
-function getSaucePrice()
-{
+function getSaucePrice() {
     var saucePrice = 0;
     var theForm = document.forms.pizza;
     var selectedSauce = theForm.elements.sauce;
@@ -240,148 +210,80 @@ function getSaucePrice()
     return saucePrice;
 }
 
-document.getElementById("sauce").addEventListener("change", function () {
-    "use strict";
-    getSaucePrice(), calculateTotal();
-});
-
-function getToppingsPrice()
-{
+function getToppingsPrice() {
     var toppingPrice = 0;
     var theForm = document.forms.pizza;
     var includeToppings = theForm.elements.pepperoni;
-    if(includeToppings.checked === true)
-    {
-        toppingPrice = toppingPrice + .99;
+    if (includeToppings.checked === true) {
+        toppingPrice = toppingPrice + 0.99;
     }
     var includeToppings = theForm.elements.sausage;
-    if(includeToppings.checked === true)
-    {
-        toppingPrice = toppingPrice + .99;
+    if (includeToppings.checked === true) {
+        toppingPrice = toppingPrice + 0.99;
     }
     var includeToppings = theForm.elements.ham;
-    if(includeToppings.checked === true)
-    {
-        toppingPrice = toppingPrice + .99;
+    if (includeToppings.checked === true) {
+        toppingPrice = toppingPrice + 0.99;
     }
     var includeToppings = theForm.elements.bacon;
-    if(includeToppings.checked === true)
-    {
-        toppingPrice = toppingPrice + .99;
+    if (includeToppings.checked === true) {
+        toppingPrice = toppingPrice + 0.99;
     }
     var includeToppings = theForm.elements.salami;
-    if(includeToppings.checked === true)
-    {
-        toppingPrice = toppingPrice + .99;
+    if (includeToppings.checked === true) {
+        toppingPrice = toppingPrice + 0.99;
     }
     var includeToppings = theForm.elements.peppers;
-    if(includeToppings.checked === true)
-    {
-        toppingPrice = toppingPrice + .99;
+    if (includeToppings.checked === true) {
+        toppingPrice = toppingPrice + 0.99;
     }
     var includeToppings = theForm.elements.olives;
-    if(includeToppings.checked === true)
-    {
-        toppingPrice = toppingPrice + .99;
+    if (includeToppings.checked === true) {
+        toppingPrice = toppingPrice + 0.99;
     }
     var includeToppings = theForm.elements.jalapenos;
-    if(includeToppings.checked === true)
-    {
-        toppingPrice = toppingPrice + .99;
+    if (includeToppings.checked === true) {
+        toppingPrice = toppingPrice + 0.99;
     }
     var includeToppings = theForm.elements.mushrooms;
-    if(includeToppings.checked === true)
-    {
-        toppingPrice = toppingPrice + .99;
+    if (includeToppings.checked === true) {
+        toppingPrice = toppingPrice + 0.99;
     } 
    var includeToppings = theForm.elements.pineapple;
-    if(includeToppings.checked === true)
-    {
-        toppingPrice = toppingPrice + .99;
+    if (includeToppings.checked === true) {
+        toppingPrice = toppingPrice + 0.99;
     }    
    var includeToppings = theForm.elements.onion;
-    if(includeToppings.checked === true)
-    {
-        toppingPrice = toppingPrice + .99;
+    if (includeToppings.checked === true) {
+        toppingPrice = toppingPrice + 0.99;
     }    
     return toppingPrice;
 }
 
-document.getElementById("pepperoni").addEventListener("change", function () {
-    "use strict";
-    getToppingsPrice(), calculateTotal();
-});
-
-document.getElementById("sausage").addEventListener("change", function () {
-    "use strict";
-    getToppingsPrice(), calculateTotal();
-});
-
-document.getElementById("ham").addEventListener("change", function () {
-    "use strict";
-    getToppingsPrice(), calculateTotal();
-});
-
-document.getElementById("bacon").addEventListener("change", function () {
-    "use strict";
-    getToppingsPrice(), calculateTotal();
-});
-
-document.getElementById("salami").addEventListener("change", function () {
-    "use strict";
-    getToppingsPrice(), calculateTotal();
-});
-
-document.getElementById("peppers").addEventListener("change", function () {
-    "use strict";
-    getToppingsPrice(), calculateTotal();
-});
-
-document.getElementById("olives").addEventListener("change", function () {
-    "use strict";
-    getToppingsPrice(), calculateTotal();
-});
-
-document.getElementById("jalapenos").addEventListener("change", function () {
-    "use strict";
-    getToppingsPrice(), calculateTotal();
-});
-
-document.getElementById("mushrooms").addEventListener("change", function () {
-    "use strict";
-    getToppingsPrice(), calculateTotal();
-});
-
-document.getElementById("pineapple").addEventListener("change", function () {
-    "use strict";
-    getToppingsPrice(), calculateTotal();
-});
-
-document.getElementById("onion").addEventListener("change", function () {
-    "use strict";
-    getToppingsPrice(), calculateTotal();
-});
-
 //  Add it all up
-function calculateTotal()
-{
+function calculateTotal() {
     var pizzaPrice = getCrustPrice() + getCheesePrice() + getSaucePrice() + getToppingsPrice();
     var priceField = document.getElementById('totalPrice');
-    priceField.style.display='block';
-    var roundedTotal = Math.round(pizzaPrice*100)/100;
-    priceField.innerHTML = "Total Pizza Cost: $"+roundedTotal;
+    priceField.style.display = 'block';
+    var roundedTotal = Math.round(pizzaPrice * 100) / 100;
+        priceField.innerHTML = "Total Pizza Cost: $" + roundedTotal;
 }
 
+document.getElementById("sizeOptions").addEventListener("change", function () {
+    "use strict";
+    calculateTotal();
+});
+
 // Copy Location Info to Billing Fields
-function copyLocationInfo()
-{
-    if(billing.checked === true)
-    document.pizzaForm.billingName.value = document.pizzaForm.name.value;
-    document.pizzaForm.billingStreet.value = document.pizzaForm.street.value;
-    document.pizzaForm.billingNumber.value = document.pizzaForm.number.value;
-    document.pizzaForm.billingCity.value = document.pizzaForm.city.value;
-    document.pizzaForm.billingState.value = document.pizzaForm.state.value;
-    document.pizzaForm.billingZip.value = document.pizzaForm.zipcode.value;
+function copyLocationInfo() {
+    if (billing.checked === true) {
+        document.pizzaForm.billingName.value = document.pizzaForm.name.value;
+        document.pizzaForm.billingStreet.value = document.pizzaForm.street.value;
+        document.pizzaForm.billingNumber.value = document.pizzaForm.number.value;
+        document.pizzaForm.billingCity.value = document.pizzaForm.city.value;
+        document.pizzaForm.billingState.value = document.pizzaForm.state.value;
+        document.pizzaForm.billingZip.value = document.pizzaForm.zipcode.value;
+    }
 }
 
 //  Credit Card Validation
@@ -393,14 +295,13 @@ function ccVerify() {
         document.pizzaForm.cardNumber.focus();
         return false;
     }
-   if (document.pizzaForm.cardNumber.value.length === 13 || document.pizzaForm.cardNumber.value.length === 15 || document.pizzaForm.cardNumber.value.length === 16) {
-       window.console.log("Valid number of digits"); 
-   }
-    else {
+    if (document.pizzaForm.cardNumber.value.length === 13 || document.pizzaForm.cardNumber.value.length === 15 || document.pizzaForm.cardNumber.value.length === 16) {
+        window.console.log("Valid number of digits");
+    } else {
         window.alert("Please enter a valid number of digits");
         document.pizzaForm.cardNumber.focus();
         return false;
-   }
+    }
 }
     
 document.getElementById("cardNumber").addEventListener("blur", function () {
@@ -411,19 +312,18 @@ document.getElementById("cardNumber").addEventListener("blur", function () {
 //  Display Card Type
 function cardType() {
     
-var list = document.getElementById("cardVendor");
+    var list = document.getElementById("cardVendor");
 // As long as <div> has a child node, remove it
-while (list.hasChildNodes()) {   
-    list.removeChild(list.firstChild);
-}  
+    while (list.hasChildNodes()) {
+        list.removeChild(list.firstChild);
+    }
     
-var cardTemp = document.pizzaForm.cardNumber.value;
-var firstNum = cardTemp.charAt(0);
-var secondNum = cardTemp.charAt(1);
+    var cardTemp = document.pizzaForm.cardNumber.value;
+    var firstNum = cardTemp.charAt(0);
+    var secondNum = cardTemp.charAt(1);
     if (firstNum == 4) {
         var whatType = "Visa";
-    }
-    else if (firstNum == 3 && secondNum == 7) {
+    } else if (firstNum == 3 && secondNum == 7) {
         var whatType = "American Express";
 }
     else if (firstNum == 5 && secondNum == 1 || firstNum == 5 && secondNum == 2 || firstNum == 5 && secondNum == 3 || firstNum == 5 && secondNum == 4 || firstNum == 5 && secondNum == 5) {
